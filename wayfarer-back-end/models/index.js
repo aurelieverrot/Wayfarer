5bbb9516b382abe3e4ecc97a6629a5a53ee7a330
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-01 16:32:34 -0700

    Add database setup

---
+++ wayfarer-back-end/models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/wayfarer';
+
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch((err) => console.log(err));
