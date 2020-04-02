75daa8d64d5513d7ad61d3231ed878ac35c3a8e8
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-04-01 20:58:34 -0700

    Edits to  styling on Header component to not affect Login and Sign Up forms

--- wayfarer-front-end/src/layout/Header/Header.js
+++ wayfarer-front-end/src/layout/Header/Header.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <header className="ui container">
+        <header className="ui container header">
             <div className="ui large secondary inverted pointing menu">
-                <a className="item"><Link to={'/'}>Wayfarer</Link></a>
+                <a className="item"><h1><Link to={'/'}>Wayfarer</Link></h1></a>
                 <div className="right item">
                     <a className="ui inverted button"><Link to={'/login'}>Log In</Link></a>
                     <a className="ui inverted button"><Link to={'/signup'}>Sign Up</Link></a>
