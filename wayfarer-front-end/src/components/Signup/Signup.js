b7213885208174b37ad95884e1cccca2d40f1583
jacob kleiman <jacobakleiman@gmail.com>
2020-04-01 21:18:16 -0700

    stashes work

--- wayfarer-front-end/src/components/Signup/Signup.js
+++ wayfarer-front-end/src/components/Signup/Signup.js
@@ -1,42 +1,90 @@
 import React from 'react';
 import './Signup.css';
+import userApi from '../../api/UserApi';
+// import { register } from '../../serviceWorker';
 class Signup extends React.Component {
 
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        city: ''
+    }
+    checkFormsEmpty = () => {
+        Object.keys(this.state.item).map(i => {
+                var myitem = this.state.item[key];
+            if (myitem == ''){
+                // this.refs.[key].parentMarkError();
+                return false
+            }
+        });
+        return true;
+    }
+    passwordValid = (password) => {
+        let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%\^&\*])(?=.{8,})")
+        return (strongRegex.test(password)) && (password.length >= 8) ? true : false
+    }
+    emailValid = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    }
+    register = () => {
+        userApi.signup()
+        .then(res => {
+            console.log(res)
+            // call function handler
+        });
+    }
+    updateState = (event) => {
+        console.log(event.target)
+    }
     onSubmit = (event) => {
         event.preventDefault();
-        console.log(event.target)
-
+        // document.querySelectorAll('.error').forEach((alert) => alert.remove());
+        let formIsValid = true;
+        // console.log(event.target.email.value)
+        // inputs = document.querySelectorAll('input')
+        // console.log(document.querySelectorAll('input'));
+        if (!this.checkFormsEmpty())
+            return
+        // if forms are non-empty
+        // validate password
+        if (!this.passwordValid(this.state.password)) 
+            formIsValid = false;
+        // validate email
+        if (!formIsValid) {
+            return
+        }
+        else {
+            this.register()
+        }
     }
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
     render() {    
         return(
             <form onSubmit={this.onSubmit} className="ui form">
                 <div className="two fields">
-                    <div className="field">
+                    <div className="field error">
                         <label>Email</label>
-                        <input type="text" placeholder="Email"/>
+                        <input onInput={this.updateState} name="email" id="email" type="text" placeholder="Email"/>
                     </div>
                     <div className="field">
                         <label>Password</label>
-                        <input type="password"/>
+                        <input onInput={this.updateState} name="password" id="password" type="password"/>
                     </div>
                 </div>
                 <div className="two fields">
                     <div className="field">
                         <label>First name</label>
-                        <input type="text" placeholder="First Name"/>
+                        <input onInput={this.updateState} name="firstName" type="text" placeholder="First Name"/>
                     </div>
                     <div className="field">
                         <label>Last name</label>
-                        <input type="text" placeholder="Last Name"/>
+                        <input onInput={this.updateState} name="lastName" type="text" placeholder="Last Name"/>
                     </div>
                 </div>
                 <div className="field">
                     <label>City</label>
-                    <input type="text" placeholder="City"/>
+                    <input onInput={this.updateState} name="city" type="text" placeholder="City"/>
                 </div>
                 <button className="ui submit button">Sign Up</button>
             </form>
