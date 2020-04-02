72cd5d14cfb7483b135d81210b608c1df787d8da
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-04-01 17:48:48 -0700

    merges with jacob branch

--- wayfarer-front-end/src/components/App/App.js
+++ wayfarer-front-end/src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Routes from '../../config/routes';
 import './App.css';
+import Header from '../../layout/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes />
     </div>
   );
--- wayfarer-front-end/src/layout/Header/Header.js
+++ wayfarer-front-end/src/layout/Header/Header.js
@@ -1,1 +1,21 @@
-// Something
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <header className="ui container">
+            <div className="ui large secondary inverted pointing menu">
+                <a className="item"><Link to={'/'}>Wayfarer</Link></a>
+                <div className="right item">
+                    <a className="ui inverted button"><Link to={'/signup'}>Sign Up</Link></a>
+                    <a className="ui inverted button"><Link to={'/signup'}>Sign Up</Link></a>
+                    {/* <a className="ui inverted button"><Link to={'/profile'}>Profile</Link></a> */}
+                </div>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
