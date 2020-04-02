a2eb9d56ff282b1e0fdfc6fe0fb1584b00dd1682
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-01 18:01:22 -0700

    Initializing BodyParser

--- wayfarer-back-end/server.js
+++ wayfarer-back-end/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const bcrypt = require('bcryptjs');
 // const session = require('express-session');
 // const MongoStore = require('connect-mongo')(session);
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const db = require('./models');
+// const db = require('./models');
 const routes = require('./routes'); // Routes Module
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
