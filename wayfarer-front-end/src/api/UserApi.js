e10b95a9e98552729cec68a796edc033d3607e50
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-02 10:37:37 -0700

    Changed auth settings to api/v1...

--- wayfarer-front-end/src/api/UserApi.js
+++ wayfarer-front-end/src/api/UserApi.js
@@ -1,14 +1,14 @@
 // Something
 import axios from 'axios';
 
-const endpoint = "http://localhost:3000"
+const endpoint = "http://localhost:4000"
 
 const login = (user) => {
-    return axios.post(endpoint+'/login',user);
+    return axios.post(endpoint+'/api/v1/login',user);
 }
 
 const signup = (user) => {
-    return axios.post(endpoint+'/register',user);
+    return axios.post(endpoint+'/api/v1/register',user);
 }
 
 export default {
