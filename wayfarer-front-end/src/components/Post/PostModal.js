bd43f9413fbae14ad1763469a7430262a18648a4
aurelieverrot <aurelie.verrot@gmail.com>
2020-04-07 15:22:56 -0700

    post submission: fields in red when empty

--- wayfarer-front-end/src/components/Post/PostModal.js
+++ wayfarer-front-end/src/components/Post/PostModal.js
@@ -16,7 +16,7 @@ class PostModal extends React.Component {
         if (field.value === '') {
             valid = false;
             // add class error to fields
-            field.classList.add('error');
+            field.parentElement.setAttribute('class', 'field error');
         }
     })
     return valid;
